dfe9969125b4d45d77a63ce6f1b1e3d5ecbbc97c
majid <majidfinn22@gmail.com>
2024-09-30 13:11:08 +0300

    majid

--- front/index.php
+++ front/index.php
@@ -8,8 +8,8 @@
     <link rel="icon" href="images/logo.png" type="image/png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
 
     <link rel="stylesheet" href="css/index.css">
 
